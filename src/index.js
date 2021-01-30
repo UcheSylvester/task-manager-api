9e0dd2f1fbc13c8ddd58f6fdcc86cb8b26e9fab6
UcheSylvester <okorocode@gmail.com>
2021-01-30 07:09:31 +0100

    fetch tasks, users, single task, single user

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.post("/users", (req, res) => {
 app.get("/users", (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch((error) => res.send(error));
+    .catch((error) => res.status(500).send(error));
 });
 
 // GET ==> users/:id
@@ -42,10 +42,15 @@ app.get("/users/:id", (req, res) => {
   const { id } = req.params;
 
   User.findById(id)
-    .then((user) => res.send(user))
-    .catch((error) => res.status(400).send(error));
+    .then((user) => {
+      if (!user) return res.status(404).send("User not found");
+
+      res.send(user);
+    })
+    .catch((error) => res.status(500).send(error));
 });
 
+// POST ==> tasks
 app.post("/tasks", (req, res) => {
   const { body } = req;
 
@@ -58,3 +63,23 @@ app.post("/tasks", (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`server is up on port ${PORT}`));
+
+// GET ==> tasks
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => res.send(tasks))
+    .catch((error) => res.status(500).send(error));
+});
+
+// GET ==> tasks/:id
+app.get("/tasks/:id", (req, res) => {
+  const { id } = req.params;
+
+  Task.findById(id)
+    .then((task) => {
+      if (!task) return res.status(404).send("Task not found");
+
+      res.send(task);
+    })
+    .catch((error) => res.status(500).send(error));
+});
